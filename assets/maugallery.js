aa619949984b5c5bfc01d77fab4bd8a82924c8db
vsabouroux <vsabouroux@aol.com>
2023-12-29 14:03:07 +0100

    Ajout de 2 images "flexhe" pour carrousel

--- assets/maugallery.js
+++ assets/maugallery.js
@@ -198,7 +198,7 @@
                             <img class="lightboxImage img-fluid" alt="Contenu de l'image affichée dans la modale au clique"/>
                             ${
                               navigation
-                                ? '<div class="mg-next" style="cursor:pointer;position:absolute;top:50%;right:-15px;background:white;}">></div>'
+                                ? '<div class="mg-next" style="cursor:pointer;position:absolute;top:50%;right:-15px;background:white;">></div>'
                                 : '<span style="display:none;" />'
                             }
                         </div>
